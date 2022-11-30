f81cdfe1a0bc81457a48e604e1d6bb8b4272643a
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2022-11-30 17:07:35 +0000

    yamaha

--- newProj.rs
+++ newProj.rs
@@ -2,11 +2,24 @@ fn main(){
 
     let bob: [i32; 4] = [1, 2, 3, 4];
     let mut i = 0;
+    let n = i + 1;
+    let mult_by_next = format!("{}", bob[i] * bob[n]);
+    let mult_by_next_bin = format!("{:b}", bob[i] * bob[n]);
 
     while i <= 3{
         println!("{}", bob[i]);
         let binary_rep = format!("{:b}", bob[i]);
         println!("{}", binary_rep);
+       
+        if i <= 2 {
+            println!("{}", mult_by_next);
+            println!("{}", mult_by_next_bin)
+        } else if i == 3 {
+           let mult_by_next = format!("{}", bob[i] * bob[i]);
+           let mult_by_next_bin = format!("{:b}", bob[i] * bob[i]);
+           println!("{}", mult_by_next);
+           println!("{}", mult_by_next_bin)
+        }
         i = i + 1;
     }
     printsmth()
---
+++ rust2boogaloo.rs
@@ -0,0 +1,25 @@
+fn main(){
+    let x = 1 * 2;
+    let xpo = 2 * 3;
+    let xpt = 3 * 4;
+    let xph = 4 * 4; 
+
+
+
+    println!("{}", 1);
+    println!("{:b}", 1);
+    println!("{}", x);
+    println!("{:b}", x);
+    println!("{}", 2);
+    println!("{:b}", 2);
+    println!("{}", xpo);
+    println!("{:b}", xpo);
+    println!("{}", 3);
+    println!("{:b}", 3);
+    println!("{}", xpt);
+    println!("{:b}", xpt);
+    println!("{}", 4);
+    println!("{:b}", 4);
+    println!("{}", xph);
+    println!("{:b}", xph);
+}
