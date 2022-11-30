4967973d8ee4d97d969c9da3961a877e0817b6e7
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2022-11-30 14:02:58 +0000

    so its rusty now

--- newProj.rs
+++ newProj.rs
@@ -1,15 +1,17 @@
 fn main(){
 
     let bob: [i32; 4] = [1, 2, 3, 4];
-    let i = 0;
+    let mut i = 0;
 
-    while i <= 4{
+    while i <= 3{
         println!("{}", bob[i]);
+        i = i + 1;
     }
     printsmth()
     
 }
 
 fn printsmth(){
-    print!("something");
+    println!("something");
+    print!("{}", ' ');
 }
